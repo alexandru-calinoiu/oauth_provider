24633b0593c16e81ab017b6546935f6cff0e236a
Calin <calinoiu.alexandru@agilefreaks.com>
2012-09-04 16:23:07 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OauthProvider::Application.config.secret_token = 'bdacd920a4284a6d38467e6824c9c50a9b6418e676d47ddc2351d57acd73c775e864b0ccccdf1b478460761a83b7e2622219313139122811d831cf022dc56646'
