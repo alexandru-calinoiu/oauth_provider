220b9f98dbeda0cc1bc4491724950a778263cb8f
Calin <calinoiu.alexandru@agilefreaks.com>
2012-09-05 11:42:45 +0300

    added some more views and doorkeeper configuration

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -2,11 +2,7 @@ Doorkeeper.configure do
   # This block will be called to check whether the
   # resource owner is authenticated or not
   resource_owner_authenticator do |routes|
-    # Put your resource owner authentication logic here.
-    # If you want to use named routes from your app you need
-    # to call them on routes object eg.
-    # routes.new_user_session_path
-    User.find_by_id(session[:user_id]) || redirect_to(routes.new_user_session_url)
+    User.find_by_id(session[:user_id]) || warden.authenticate!(:scope => :user)
   end
 
   # If you want to restrict the access to the web interface for
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 OauthProvider::Application.routes.draw do
 
+  get "home/index"
+
   mount Doorkeeper::Engine => '/oauth'
 
   devise_for :users
 
+  root :to => "home#index"
 end
