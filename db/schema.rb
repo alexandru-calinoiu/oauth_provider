2e9787f2bfb3b664829d535982226edc19d29398
Calin <calinoiu.alexandru@agilefreaks.com>
2012-09-06 14:23:57 +0300

    chanced auth to work with username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120905124440) do
+ActiveRecord::Schema.define(:version => 20120906105407) do
 
   create_table "client_applications", :force => true do |t|
     t.string   "name"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(:version => 20120905124440) do
     t.datetime "updated_at",                             :null => false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
