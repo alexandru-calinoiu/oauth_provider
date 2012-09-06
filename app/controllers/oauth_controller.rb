5d01acb75e5c1ade8b0f64c64e2c2ec2380cb4c4
Calin <calinoiu.alexandru@agilefreaks.com>
2012-09-06 09:56:25 +0300

    skiped the authorization part, we consider all requests as being authorized

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -16,11 +16,41 @@ class OauthController < ApplicationController
   end
 
   protected
+
+  def oauth1_authorize
+    unless @token
+      render :action=>"authorize_failure"
+      return
+    end
+
+    unless @token.invalidated?
+      if user_authorizes_token?
+        @token.authorize!(current_user)
+        callback_url  = @token.oob? ? @token.client_application.callback_url : @token.callback_url
+        @redirect_url = URI.parse(callback_url) unless callback_url.blank?
+
+        unless @redirect_url.to_s.blank?
+          @redirect_url.query = @redirect_url.query.blank? ?
+              "oauth_token=#{@token.token}&oauth_verifier=#{@token.verifier}" :
+              @redirect_url.query + "&oauth_token=#{@token.token}&oauth_verifier=#{@token.verifier}"
+          redirect_to @redirect_url.to_s
+        else
+          render :action => "authorize_success"
+        end
+      else
+        @token.invalidate!
+        render :action => "authorize_failure"
+      end
+    else
+      render :action => "authorize_failure"
+    end
+  end
+
   # Override this to match your authorization page form
   # It currently expects a checkbox called authorize
-  # def user_authorizes_token?
-  #   params[:authorize] == '1'
-  # end
+  def user_authorizes_token?
+     true
+  end
 
   # should authenticate and return a user if valid password.
   # This example should work with most Authlogic or Devise. Uncomment it
